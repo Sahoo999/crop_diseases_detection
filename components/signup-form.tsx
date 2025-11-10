9e08ab4da3a77bbc85f7b81289bb726da3248d92
Sahoo999 <sahoodeb04@gmail.comm>
2025-11-10 23:39:52 +0530

    npmrc

--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -43,7 +43,7 @@ export function SignupForm() {
           <div className="relative">
             <User className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-muted-foreground" />
             <Input
-              id="name"
+              id="name" 
               type="text"
               placeholder="John Doe"
               value={name}
